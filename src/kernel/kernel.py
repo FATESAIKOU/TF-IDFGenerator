c82aae08a03900313de999a66ec2e42b792592e5
FATESAIKOU <qzsecftbhhhh@gmail.com>
2016-06-14 12:22:04 +0800

    kernel/Utils gpudiff 50%

--- src/kernel/kernel.py
+++ src/kernel/kernel.py
@@ -1,31 +1,54 @@
 import numpy
 import sys
 import pprint
+import time
 
 import utils as ut
 
 def upload(filename, threshold):
+  start = time.time()
 
   """ get Words from provided file """
   words = ut.getWords(filename)
+  end = time.time()
+  print 'Get words time:', end - start
+  start = end
 
   """ get TF array and word number with Words"""
   (word_num, tf) = ut.genTFArray(words)
+  end = time.time()
+  print 'Get TF time:', end - start
+  start = end
 
   """ get TF-IDF array with tf & word_num """
   tf_idf = ut.genTF_IDFArray(tf, word_num)
+  end = time.time()
+  print 'Get TF-IDF time:', end - start
+  start = end
 
   """ create Book record at mysql database and get the record """
   book_rec = ut.createBookRecord(tf_idf, filename)
+  end = time.time()
+  print 'create Book time:', end - start
+  start = end
 
   """ get all similar class & books with tf_idf & threshold """
   (sim_classes, sim_books) = ut.getSim(book_rec, tf_idf, threshold)
+  end = time.time()
+  print 'Get Sim time:', end - start
+  start = end
 
   """ append to the similar class """
   ut.appendToSimClass(sim_classes, book_rec)
+  end = time.time()
+  print 'Append to sim class time:', end - start
+  start = end
 
   """ if sim book_num more the one, than create a new class """
   if len(sim_books) > 1:
     ut.createClassRecord(sim_books, tf_idf)
+  end = time.time()
+  print 'create class time:', end - start
+  start = end
 
   return 0
--- src/kernel/utils.py
+++ src/kernel/utils.py
@@ -10,7 +10,8 @@ from math import sqrt
 
 import numpy as np
 import pycuda.autoinit
-import pycuda.driver as drv
+import pycuda.driver as cuda
+from pycuda.compiler import SourceModule
 
 sys.path.insert(0, '/home/fatesaikou/testPY/TF-IDFGenerator/src')
 from config import *
@@ -61,7 +62,10 @@ def genTF_IDFArray(tf, word_num):
   r = redis.Redis()
   tf_idf = [0.0] * (word_num + 1)
   for word_id, count in enumerate(tf):
-    tf_idf[word_id] = sqrt(float(count) / float(word_num)) * float(r.hget('idfs', word_id))
+    if count > 0:
+      tf_idf[word_id] = sqrt(float(count) / float(word_num)) * float(r.hget('idfs', word_id))
+    else:
+      tf_idf[word_id] = 0
 
   return tf_idf
 
@@ -167,7 +171,9 @@ def saveTF_IDF(type, id, tf_idf):
 def getSim(book_rec, tf_idf, threshold):
   db = MySQLdb.connect(mysql['host'], mysql['username'], mysql['password'], mysql['database'])
   cursor = db.cursor()
+  r = redis.Redis()
 
+  size = int(r.hget('words', '__total__')) + 1
   np_tf_idf = np.array(tf_idf)
   sim_classes = []
   sim_books = []
@@ -177,17 +183,28 @@ def getSim(book_rec, tf_idf, threshold):
   for class_item in cursor.fetchall():
     class_id = class_item[0]
     c_tf_idf = readTF_IDF('class', class_id)
-    cos_diff = getDiff(tf_idf, c_tf_idf)
+
+    np_c_tf_idf = np.array(c_tf_idf).astype(np.float32)
+    np_c_tf_idf.resize(size)
+    cos_diff = getDiff(np_tf_idf, np_c_tf_idf)
+
     if cos_diff < threshold:
-      c_tf_idf = ( (np.array(c_tf_idf) * class_item[1] + np_tf_idf) / float(class_item[1]) ).tolist()
+      c_tf_idf = ( (np_c_tf_idf * class_item[1] + np_tf_idf) / float(class_item[1] + 1) ).tolist()
       sim_classes.append( (class_id, c_tf_idf, cos_diff) )
       class_id_str += ',' + str(class_id)
 
-  cursor.execute('SELECT `books`.`id` FROM `books` LEFT JOIN `book_class` on `book_class`.`b_id` = `books`.`id` and `book_class`.`c_id` NOT IN (' + class_id_str + ')')
+  cursor.execute('SELECT distinct(`books`.`id`) FROM `books` LEFT JOIN `book_class` on `book_class`.`b_id` = `books`.`id` and `book_class`.`c_id` NOT IN (' + class_id_str + ')')
   for book_item in cursor.fetchall():
+    if book_item[0] == book_rec['id']:
+      continue
+
     book_id = book_item[0]
     b_tf_idf = readTF_IDF('book', book_id)
-    cos_diff = getDiff(tf_idf, b_tf_idf)
+
+    np_b_tf_idf = np.array(b_tf_idf)
+    np_b_tf_idf.resize(size)
+    cos_diff = getDiff(np_tf_idf, np_b_tf_idf)
+
     if cos_diff < threshold:
       sim_books.append((book_id, cos_diff))
 
@@ -195,9 +212,61 @@ def getSim(book_rec, tf_idf, threshold):
 
   return (sim_classes, sim_books)
 
-def getDiff(a, b):
-  max_len = max(len(a), len(b))
-  np_a = np.array(a + ([0] * (max_len - len(a))))
-  np_b = np.array(b + ([0] * (max_len - len(b))))
-
+def getDiff(np_a, np_b):
   return sum(np_a * np_b) / float(sqrt(sum(np_a**2)) + sqrt(sum(np_b**2)))
+
+def getDiffGpu(np_a, np_b):
+  mod = SourceModule("""
+    __global__ void mult(float *dest, float *a, float *b, int limit)
+    {
+      int jump_size = blockDim.x * gridDim.x;
+      int offset = threadIdx.x + blockDim.x * blockIdx.x;
+
+      int index;
+      for (index = offset; index < limit; index += jump_size) {
+        dest[index] = a[index] * b[index];
+      }
+    }
+
+    __global__ void power2(float *dest, float *a, int limit)
+    {
+      int jump_size = blockDim.x * gridDim.x;
+      int offset = threadIdx.x + blockDim.x * blockIdx.x;
+
+      int index = offset;
+      //for (index = offset; index < limit; index += jump_size) {
+      if (index < 0) {
+        dest[index] = a[index] * a[index];
+      }
+    }
+  """)
+
+  mult = mod.get_function("mult")
+  power = mod.get_function("power2")
+
+  a_gpu = cuda.mem_alloc(np_a.nbytes)
+  b_gpu = cuda.mem_alloc(np_b.nbytes)
+  mult_res_gpu = cuda.mem_alloc(np_a.nbytes)
+
+  cuda.memcpy_htod(a_gpu, np_a)
+  cuda.memcpy_htod(b_gpu, np_b)
+  mult(mult_res_gpu, a_gpu, b_gpu, np.int32(np_a.size), block = (1024, 1, 1), grid = (1, 1, 1))
+  #power(a_gpu, a_gpu, np.int32(np_a.size), block = (256, 1, 1), grid = (128, 1, 1))
+  #power(b_gpu, b_gpu, np.int32(np_b.size), block = (256, 1, 1), grid = (128, 1, 1))
+
+  #pow_a = np.zeros_like(np_a)
+  #pow_b = np.zeros_like(np_b)
+  mult_res = np.zeros_like(np_a)
+
+  #cuda.memcpy_dtoh(pow_a, a_gpu)
+  #cuda.memcpy_dtoh(pow_b, b_gpu)
+  cuda.memcpy_dtoh(mult_res, mult_res_gpu)
+  pprint.pprint(mult_res)
+
+  #return mult_res.sum() / float( sqrt(pow_a.sum()) + sqrt(pow_b.sum()) )
+
+
+
+
+
+
